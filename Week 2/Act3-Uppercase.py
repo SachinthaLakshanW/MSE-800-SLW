30e82badb01d99c447a7c8860a1618fbd1dc3688
SachinthaLakshanW <sachinthalakshan333@gmail.com>
2025-08-10 11:29:31 +1200

    3rd Com

--- Week 2/Act3-Uppercase.py
+++ Week 2/Act3-Uppercase.py
@@ -1,27 +1,25 @@
-
 class StringManipulator:
     def __init__(self, text):
         self.text = text
 
     def find_character(self, char):
-        return self.text(char)
+    
+        return self.text.find(char)
     
     def find_length(self):
         return len(self.text)   
     
-    def find_uppercase(self):
-        return len(self.text)   
-    #create an instance of the StringManipulator class
-    
+    def to_uppercase(self):
+        return self.text.upper()
+
+# create an instance of the StringManipulator class
 name = StringManipulator("Example")
 
-    
-    #Call the find_character method on the object
-result = name.find_character('x')
-result = name.find_length()
-result = name.find_uppercase()
+# Call the methods
+char_index = name.find_character('x')
+length = name.find_length()
+upper_text = name.to_uppercase()
 
-
-print(result)
-
-        
+print("Index of 'x':", char_index)
+print("Length:", length)
+print("Text in uppercase:", upper_text)
