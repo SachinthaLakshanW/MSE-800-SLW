941bf00f6c471f4e3f3d0f35cb106c1a3c429df0
SachinthaLakshanW <sachinthalakshan333@gmail.com>
2025-10-18 08:34:43 +1300

    Adding NAother page to flask

--- Week 12/act1.py
+++ Week 12/act1.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def hello_flask():
     return "<h1>Hello, Flask</h1>"
 
+@app.route("/bye")
+def hello_Bye():
+    return "<h1>Bye, Flask</h1>"
+
 if __name__ == "__main__":
     app.run(debug=True)
